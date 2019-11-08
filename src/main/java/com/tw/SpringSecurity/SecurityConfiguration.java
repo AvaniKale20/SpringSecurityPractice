412f2d3c165023179a7608522069b9f8663a4b93
avanijayantkale <Avanikale20>
2019-11-08 14:54:13 +0530

    Add. can show sign in page with multiple user.

--- src/main/java/com/tw/SpringSecurity/SecurityConfiguration.java
+++ src/main/java/com/tw/SpringSecurity/SecurityConfiguration.java
@@ -12,7 +12,17 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication().withUser("avani").password("avani").roles("USER");
+        auth.inMemoryAuthentication().
+                withUser("avani").
+                password("avani").
+                roles("USER").
+                and().
+                withUser("shruti").
+                password("shruti").roles("ADMIN").
+                and()
+                .withUser("user-one").
+                password("user-one").
+                roles("HR");
     }
     //not assuming password as string want encoded password i.e we have to write getPassword() method to return encoded password
 
