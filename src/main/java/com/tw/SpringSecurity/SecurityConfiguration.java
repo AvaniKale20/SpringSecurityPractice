6ed593d42c2badc3fab46dfa7c25315e0ea63ada
avanijayantkale <Avanikale20>
2019-11-08 14:49:25 +0530

    Add. can show sign in page without using application properties and overriding configure method of authentication.

---
+++ src/main/java/com/tw/SpringSecurity/SecurityConfiguration.java
@@ -0,0 +1,23 @@
+package com.tw.SpringSecurity;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity//this tells
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().withUser("avani").password("avani").roles("USER");
+    }
+    //not assuming password as string want encoded password i.e we have to write getPassword() method to return encoded password
+
+    @Bean
+    public PasswordEncoder getPassword() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+}
